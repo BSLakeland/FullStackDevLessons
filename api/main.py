35b60ab008431a7371ffe44c43b09103a4d6839d
Ben Lakeland <b.lakeland1998@gmail.com>
2023-02-17 15:36:27 +0000

    Updated dependencies to add jinja2

--- api/main.py
+++ api/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from datetime import datetime
 import os
 app = FastAPI()
 
-@app.get("/")
+@app.get("/time", response_class=HTMLResponse)
 async def homepage():
     time = datetime.now()
     message = f"the time is {time}"
     os.system(f"say '{message}'")
-    return message
+    return f"<h1>{message}</h1>"
 
-@app.get('/hello/{name}')
+@app.get('/hello/{name}', response_class=HTMLResponse)
 async def hello_name(name: str):
-    return f'Hello {name}'
+    return f'<h1>Hello {name}</h1>'
