3dea031f724414a98481ca94a32a04ad4d51546b
Ben Lakeland <b.lakeland1998@gmail.com>
2023-02-17 15:33:11 +0000

    Added example of fast api to get web server

--- api/main.py
+++ api/main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
-
+from datetime import datetime
+import os
 app = FastAPI()
+
+@app.get("/")
+async def homepage():
+    time = datetime.now()
+    message = f"the time is {time}"
+    os.system(f"say '{message}'")
+    return message
+
+@app.get('/hello/{name}')
+async def hello_name(name: str):
+    return f'Hello {name}'
